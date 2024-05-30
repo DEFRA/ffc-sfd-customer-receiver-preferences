241b2f1a54e69e5b8b0b5a43f94b8e5b5978d742
Rana Salem <ranatasalem@gmail.com>
2024-05-30 14:32:28 +0100

    update cosmos config to support no cosmos

--- app/config/cosmos.js
+++ app/config/cosmos.js
@@ -3,15 +3,15 @@ const Joi = require('joi')
 const schema = Joi.object({
   endpoint: Joi.string(),
   key: Joi.string(),
-  preferencesDatabase: Joi.string().required(),
-  preferencesContainer: Joi.string().required()
+  preferencesDatabase: Joi.string().default('ffc-sfd-customer-receiver-preferences'),
+  preferencesContainer: Joi.string().default('preferences-container')
 })
 
 const config = {
   endpoint: process.env.COSMOS_ENDPOINT,
   key: process.env.COSMOS_KEY,
-  preferencesDatabase: 'ffc-sfd-customer-receiver-preferences',
-  preferencesContainer: 'preferences-container'
+  preferencesDatabase: process.env.COSMOS_PREFERENCES_DATABASE || 'ffc-sfd-customer-receiver-preferences',
+  preferencesContainer: process.env.COSMOS_PREFERENCES_CONTAINER || 'preferences-container'
 }
 
 const { error, value } = schema.validate(config, { abortEarly: false })
